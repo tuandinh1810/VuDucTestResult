57229ff35136ed7c5933b92ebdaaa136be0f735c
Nguyen Dinh Tuan <tuan.nguyen-dinh@tek-experts.com>
2016-08-01 18:14:42 +0700

    commit VuDucTestResult source code

---
+++ VuDucLabConn/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VuDucLabConn.MvcApplication" Language="C#" %>
---
+++ VuDucLabConn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VuDucLabConn.Startup))]
+namespace VuDucLabConn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
